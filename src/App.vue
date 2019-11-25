056a212ea91057d2bea2d7ebd76450267108e0db
kariukisamuel <inboxsamuel@outlook.com>
2019-11-25 07:07:49 +0300

    My First Commit

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+    <div id="app" @mouseover="mouseOver">
+        <transition-group name="fade-in" v-if="hovered" appear tag="div" class="parent-box" >
+            <ChildBox v-for="(title,index) in titles" :item="title" :key="'child' + index" :index="index" />
+        </transition-group>
+    </div>
+</template>
+<script>
+import ChildBox from './components/ChildBox.vue'
+
+export default {
+    name: 'app',
+    data() {
+        return {
+            titles: [
+                { text: 'Box-A', color: '#F9423A' },
+                { text: 'Box-B', color: '#707372' },
+                { text: 'Box-C', color: '#2CD5C4' }
+            ],
+            hovered: false
+        }
+    },
+    components: {
+        ChildBox
+    },
+    methods: {
+        mouseOver() {
+            this.hovered = true;
+        }
+    }
+}
+</script>
+<style lang="scss">
+#app{
+  height:100vh
+}
+.fade-in-enter {
+    opacity: 0;
+    transform: scale(.5);
+}
+
+.fade-in-enter-active {
+    transition: all .8s ease-in;
+    transition-delay: calc(.2s * var(--i));
+}
+
+.parent-box {
+    display: flex;
+    width: 100%;
+    justify-content: space-around;
+    background: aliceblue;
+    padding: 5%0;
+}
+</style>
---
+++ src/components/ChildBox.vue
@@ -0,0 +1,35 @@
+<template>
+
+    <div class="child-box" :style="boxStyle">
+        <p >{{ item.text }}</p>
+    </div>
+</template>
+<script>
+export default {
+    name: 'ChildBox',
+    props: ['item', 'index'],
+    computed:{
+      boxStyle(){
+          return{
+           '--i': this.index,
+           'backgroundColor': this.item.color
+          }
+      }
+     
+    }
+
+}
+</script>
+<style scoped lang="scss">
+.child-box {
+    width: 350px;
+    height: 50vh;
+    align-items: center;
+    justify-content: center;
+    display:flex;
+    p{
+      color:#fff;
+      font-size:20px;
+    }
+}
+</style>
